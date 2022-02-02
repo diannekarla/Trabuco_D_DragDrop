0beb77e9e3246cb96d11f566fdb88ca8293009d4
Dianne Karla Trabuco <diannekarlatrabuco@Diannes-MacBook-Air.local>
2022-02-02 12:55:05 -0500

    adding drag and drop

--- script/main.js
+++ script/main.js
@@ -2,14 +2,16 @@
 	// make the  connections to the elements on the page
 	// that we want the user to interact with
 	const theButtons = document.querySelectorAll("#buttonHolder img"),
+				puzzlePieces = document.querySelectorAll(".puzzle-pieces *"),
+				dropZones = document.querySelectorAll(".drop-zone"),
 				theGameBoard = document.querySelector(".puzzle-board");
 
 	// thebuttons becomes this:
 	// [
-	// <img>
-	// <img>
-	// <img>
-	// <img>
+	// <img src="images/buttonZero.jpg" data-bgref="0" alt="thumbnail">
+	// <img src="images/buttonOne.jpg" data-bgref="1" alt="thumbnail">
+	// <img src="images/buttonTwo.jpg" data-bgref="2" alt="thumbnail">
+	// <img src="images/buttonThree.jpg" data-bgref="3" alt="thumbnail">
 	// ]
 
 		function changeBgImg() {
@@ -24,8 +26,27 @@
 			// search for MDN Javascript Template String
 		}
 
+		function startDrag(event){
+			console.log("started dragging");
+		}
+
+		function draggedOver(event){
+			event.preventDefault();
+			console.log("dragged me over");
+		}
+
+		function handleDrop(event){
+			event.preventDefault();
+			console.log("dropped on me");
+		}
+
 		// these are the "triggers" we want to use to fire off events
 		theButtons.forEach(button => button.addEventListener("click", changeBgImg));
 
+		puzzlePieces.forEach(piece => piece.addEventListener("dragstart", startDrag));
+		dropZones.forEach(zone => {
+			zone.addEventListener("dragover", draggedOver);
+			zone.addEventListener("drop", handleDrop);
+		});
 
 })();
